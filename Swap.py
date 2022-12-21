2a75ccd9efe7eb415a083992c616e3e8dee7896d
saisg094 <119469698+saisg094@users.noreply.github.com>
2022-12-21 15:45:54 -0600

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,9 @@
+#Swap two variables
+
+P = int(input("Enter the value of P:"))
+Q = int(input("Enter the value of Q:"))
+temp_1 = P
+P = Q
+Q = temp_1
+print("Print the value of P after swapping:", P)
+print("Print the value of Q after swapping:", Q)
