46adb91bd325cf3952e7b42e705491fa7d6fd84e
saisg094 <119469698+saisg094@users.noreply.github.com>
2022-12-21 15:16:14 -0600

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,20 @@
+print("Hello Kansas City")
+
+print('7')
+
+if 10 == 10:
+    print('something')
+
+
+#Finding the area of the triangle
+
+a = float(input('enter the first side:'))
+b = float(input('enter the second side:'))
+c = float(input('enter the third side:'))
+s = (a+b+c) / 2
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+print('The area of the triangle is %0.2f'%area)
+
+
+
+
